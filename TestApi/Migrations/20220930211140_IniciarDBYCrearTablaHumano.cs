d8d71ca6d24577ff3bbe09d7ac53a8b6da177518
Luis Jose Padilla Benitez <lupadilla@bansi.com.mx>
2022-09-30 17:36:12 -0500

    Add Humano table migration #3

---
+++ TestApi/Migrations/20220930211140_IniciarDBYCrearTablaHumano.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApi.Migrations
+{
+    public partial class IniciarDBYCrearTablaHumano : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "humanos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Sexo = table.Column<string>(type: "TEXT", maxLength: 6, nullable: false),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Altura = table.Column<float>(type: "REAL", nullable: false),
+                    Peso = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_humanos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "humanos");
+        }
+    }
+}
