e0ba83344107873ffd8faa41685b478f72cde173
Luis Jose Padilla Benitez <lupadilla@bansi.com.mx>
2022-09-30 18:26:11 -0500

    Add the respective methods for GET, POST, PUT to HumanosController #5

--- TestApi/Controllers/HumanosController.cs
+++ TestApi/Controllers/HumanosController.cs
@@ -11,26 +11,36 @@ public class HumanosController : ControllerBase
 {
     private readonly ILogger<HumanosController> _logger;
     private readonly TestApiContext _context;
+    private readonly Mock<IGenericRepository<Humano>> _mockRepository;
 
     public HumanosController(ILogger<HumanosController> logger, TestApiContext context)
     {
         _context = context;
         _logger = logger;
+        _mockRepository = new Mock<IGenericRepository<Humano>>();
+        _mockRepository.Setup(m => m.GetAll()).Returns(_context.humanos.ToList());
+        _mockRepository.Setup(m=> m.GetById(It.IsAny<int>())).Returns((int idValue)=> _context.humanos.Find(idValue));
+        _mockRepository.Setup(m=> m.Insert(It.IsAny<Humano>())).Callback((Humano h)=>{ _context.humanos.Add(h);});
+        _mockRepository.Setup(m => m.Save()).Callback(()=>{_context.SaveChanges(); });
     }
 
     [HttpGet]
     public IEnumerable<Humano> Get()
     {
-        Mock<IGenericRepository<Humano>> mockRepository = new Mock<IGenericRepository<Humano>>();
-        mockRepository.Setup(m => m.GetAll()).Returns(_context.humanos.ToList());
-        return mockRepository.Object.GetAll();
+        return _mockRepository.Object.GetAll();
     }
 
     [HttpGet("{id}")]
     public Humano GetById(int id)
     {
-        Mock<IGenericRepository<Humano>> mockRepository = new Mock<IGenericRepository<Humano>>();
-        mockRepository.Setup(m=> m.GetById(It.IsAny<int>())).Returns((int idValue)=> _context.humanos.Find(idValue));
-        return mockRepository.Object.GetById(id);
+        return _mockRepository.Object.GetById(id);
+    }
+
+    [HttpPost]
+    public ActionResult Create(Humano humano)
+    {
+        _mockRepository.Object.Insert(humano);
+        _mockRepository.Object.Save();
+        return Ok();
     }
 }
